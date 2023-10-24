d9094b6150753c68f98f882d6afeab33112fa0f0
Barna Dudok <barna.dudok@outlook.com>
2023-10-24 11:50:51 -0500

    Export Stop is implemented in new pipeline

--- Proc2P/Bruker/LoadEphys.py
+++ Proc2P/Bruker/LoadEphys.py
@@ -4,7 +4,11 @@ class Ephys:
     '''to interface with spike detector (emulate legacy ripples.trace)'''
 
     def __init__(self, procpath, prefix, channel=0):
-        self.edat = numpy.load(os.path.join(procpath, prefix, prefix + '_ephys.npy'))
-        self.frames = self.edat[0]
-        if channel is not None:
-            self.trace = self.edat[channel+1].astype('float') / 1000
+        epn = os.path.join(procpath, prefix, prefix + '_ephys.npy')
+        if os.path.exists(epn):
+            self.edat = numpy.load(epn)
+            self.frames = self.edat[0]
+            if channel is not None:
+                self.trace = self.edat[channel+1].astype('float') / 1000
+        else:
+            self.trace = None
