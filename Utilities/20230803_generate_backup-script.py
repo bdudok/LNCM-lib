84a38de3e8820e902df257a3e9bb269e01d7d66f
Barna Dudok <barna.dudok@outlook.com>
2023-11-10 11:06:06 -0600

    database included in daily backup

--- Utilities/20230803_generate_backup-script.py
+++ Utilities/20230803_generate_backup-script.py
@@ -3,7 +3,7 @@ import win32api, win32file
 
 '''Back up raw data folders to onedrive
 The script is scheduled to run on the server nightly.
-update to include/exclude folders inthe backup.
+update to include/exclude folders in the backup.
 
 for installing pywin32:
 
@@ -12,6 +12,7 @@ cd C:\ProgramData/anaconda3\envs\suite2p\Scripts
 python pywin32_postinstall.py -install
 '''
 script_handle = 'C:/Users/u247640/OneDriveBackup.cmd'
+ps_script_handle = 'C:/Users/u247640/DBBackup.ps1'
 
 dest_path = 'OneDrive:_RawData/'
 source_path = 'D:\Shares\Data\_RawData/'
@@ -44,9 +45,19 @@ ext_drive_name = 'LNCM1'
 if ext_drive_name in drive_names:
     dest_path = f'E:\ExtDrives/{ext_drive_name}/_Processed/'
     source_path = 'D:\Shares\Data\_Processed/'
-    dlist = ['2P/PVTot']
+    dlist = ['2P/PVTot', '2P/SncgTot']
     for d in dlist:
         script_s += get_cmd(d)
+
+#raw 2P data to LNCM2
+ext_drive_name = 'LNCM2'
+if ext_drive_name in drive_names:
+    dest_path = f'E:\ExtDrives/{ext_drive_name}/_RawData/'
+    source_path = 'D:\Shares\Data\_RawData/'
+    dlist = ['Bruker']
+    for d in dlist:
+        script_s += get_cmd(d)
+
 
 #DSI revision analysis to OneDrive
 dest_path = 'OneDrive:Documents/_projects/2023-DSI/Revision_analysis/'
@@ -55,8 +66,40 @@ dlist = ['Revision']
 for d in dlist:
     script_s += get_cmd(d)
 
+#back up baserow db
+dest_path = 'OneDrive:_baserow_backup/'
+source_path = 'D:\Shares\Data\DB_Backups/'
+dlist = ['30days']
+for d in dlist:
+    script_s += get_cmd(d)
+
+#keep last 10 daily backups, then every 30 days
+ps_script = '''cd D:\Shares\Data\DB_Backups
+mv *.tar ./10days/
+docker stop baserow
+docker run --rm -v baserow_data:/baserow/data -v ${PWD}:/backup ubuntu tar cvf /backup/baserow_$(get-date -f yyyy-MM-dd).tar /baserow/data
+docker start baserow
+$oldestFile = gci ./10days/ | select -first 1
+$latestBu = gci ./30days/ | select -last 1
+$timespanF = new-timespan -days 10
+$timespanB = new-timespan -days 30
+if (((get-date) - $oldestFile.LastWriteTime) -gt $timespanF) {
+    if (((get-date) - $latestBu.LastWriteTime) -gt $timespanB) {
+        mv ./10days/$oldestFile ./30days/
+    }
+    else {rm ./10days/$oldestFile}
+}
+'''
+
+script_s += ps_script_handle + '\n'
 
 with open(script_handle, 'w') as f:
     f.write(script_s)
 
+with open(ps_script_handle, 'w') as f:
+    f.write(ps_script)
+
+print('--------------batch file for rclone backups----------------')
 print(script_s)
+print('--------------PS script for baserow backup-----------------')
+print(ps_script)
