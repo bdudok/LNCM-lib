5f7c54b6a3a49ff7dbdc592e15f1da38582450b8
Barna Dudok <barna.dudok@outlook.com>
2024-02-19 12:10:02 -0600

    Mouse list update fixed for multipage response

--- Utilities/UpdateMice.py
+++ Utilities/UpdateMice.py
@@ -8,28 +8,48 @@ lg_api_token = "c0aa7d060d092ca76324001e5326950f38b6f9d3"
 # lg_api_token = config["lg_token"]
 
 #get list of mice from LG
-resp = requests.get(f'{config["lg_mice_url"]}?token={lg_api_token}')
-mice_lg = pandas.DataFrame(resp.json())
+pagesize=100
+mice_lg = []
+next_page = 1
+while next_page:
+    resp = requests.get(f'{config["lg_mice_url"]}?token={lg_api_token}',
+                        params={'page_size': pagesize, 'page': next_page})
+    rjs = resp.json()
+    mice_lg.append(pandas.DataFrame(rjs))
+    if len(rjs) < pagesize:
+        next_page = False
+    else:
+        next_page += 1
+
+mice_lg = pandas.concat(mice_lg)
+print('Got mouse list from LabGuru')
 
 #get list of mice from BR (can be multipage)
 auth_string = f"Token {config['api_token']}"
 mice_br = []
-for page in range(int(len(mice_lg)/100+10)):
+next_page = 1
+while next_page:
     resp = requests.get(config['mice_url'],
                         headers={"Authorization": auth_string},
-                        params={'page': page}
+                        params={'page_size': pagesize, 'page': next_page}
                         )
     if resp.status_code == 200:
         mice_br.append(pandas.DataFrame(resp.json()['results']))
         if resp.json()['next'] is None:
-            break
+            next_page = False
+        else:
+            next_page += 1
+print('Got mouse list from Baserow')
 
 mice_br = pandas.concat(mice_br)
 #find diff
 lg_set = set(mice_lg['name'])
 br_set = set(mice_br['Mouse.ID'])
+print(f'{len(lg_set)} in LabGuru, {len(br_set)} in Baserow')
 diff_mouse = list(lg_set.difference(br_set))
 diff_mouse.sort()
+if not len(diff_mouse):
+    print(f'No new mouse')
 
 # put new mouse in BR
 for mouse in diff_mouse:
